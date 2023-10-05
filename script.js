09e6ea7b6e5664909f7ea44c498c4e20599135f9
BlackFury117 <idrissadiallo97@icloud.com>
2023-10-05 16:06:03 +0000

    Fixing the hamburger menu

--- script.js
+++ script.js
@@ -8,6 +8,26 @@ $(document).on("click", function(event){
   // Prevent the dropdown from closing when clicking inside it
   $(".dropdown-link").on("click", function(event){
     event.stopPropagation();
+});
+
+
+// MOBILE NAVIGATION //
+
+document.addEventListener("DOMContentLoaded", function() {
+  const hamburgerCheckbox = document.querySelector(".hamburger input");
+  const navLinks = document.querySelector(".dropdown-links");
+
+  hamburgerCheckbox.addEventListener("change", function() {
+    if (this.checked) {
+      navLinks.style.display = "inline-flex";
+      document.body.style.overflow = "hidden";
+      navLinks.classList.add("show");
+    } else {
+      navLinks.style.display = "none";
+      document.body.style.overflow = "auto";
+      navLinks.classList.remove("show");
+    }
   });
+});
 
-
+// END MOBILE NAVIGATION
